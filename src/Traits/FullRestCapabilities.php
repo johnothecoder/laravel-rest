a85511332f87b5a1f31b342b9a6edd23a0324150
Matt Johnson <mattjohnsonuk@icloud.com>
2019-02-02 22:45:34 +0000

    Added 2x shortcut traits for full rest capabilities, and read only - just to make userland code that little easier

---
+++ src/Traits/FullRestCapabilities.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace KyaSoftware\LaravelRest\Traits;
+
+/**
+ * Trait FullRestCapabilities
+ * @package KyaSoftware\LaravelRest\Traits
+ */
+trait FullRestCapabilities
+{
+    use HasRestModel, DoesDestroy, DoesPatch, DoesPut, DoesSearch, DoesStore;
+}
---
+++ src/Traits/ReadOnly.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace KyaSoftware\LaravelRest\Traits;
+
+/**
+ * Trait ReadOnly
+ * @package KyaSoftware\LaravelRest\Traits
+ */
+trait ReadOnly
+{
+    use HasRestModel, DoesSearch;
+}
