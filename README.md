6b0df433d0ac009ac29418a5b534e8bc679a7b5d
Matt Johnson <mattjohnsonuk@icloud.com>
2019-02-02 21:22:00 +0000

    Working on the core GET POST PUT PATCH DELETE functionality - abstractions pending

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 An open source package for #laravel to add easy #rest functionality to your controllers
 
-This package is currently under construction
+This package is currently under construction - installation on any project, whether for development or production, is strongly discouraged at this time
--- src/Controllers/RestController.php
+++ src/Controllers/RestController.php
@@ -4,17 +4,39 @@ namespace KyaSoftware\LaravelRest\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use KyaSoftware\LaravelRest\Handlers\Search\SearchHandler;
 
 abstract class RestController extends Controller
 {
 
-    public function list(Request $request)
+    public function index(Request $request)
     {
+
         $class = $this->getModelClassName();
+        $this->verifyIndexAccess();
         $query = $class::select($this->getDefaultSelect());
         $this->handleFiltering($request, $query);
         $this->handleSearch($request, $query);
+        $this->handleAdditionalFiltering($request, $query);
+        $this->handleOrdering($request, $query);
+        $this->handleTrash($request, $query);
+        if($this->usePagination() === true){
+            $perPageField = $this->getPerPageField();
+            $results = $query->paginate($request->$perPageField ?? 25)->appends(request()->query());
+        } else {
+            $results = $query->get();
+        }
 
+        foreach($results as &$result){
+            $this->buildModelLinks($result);
+        }
+
+        return response()->json($results);
+    }
+
+    protected function buildModelLinks(&$model)
+    {
+        $model->links = [];
     }
 
     protected function handleFiltering(Request $request, &$query)
@@ -41,29 +63,155 @@ abstract class RestController extends Controller
         }
     }
 
+    protected function handleOrdering(Request $request, &$query)
+    {
+        $orderByField = $this->getOrderByField();
+        $orderField = $this->getOrderField();
+        $query->orderBy(
+            $request->$orderByField ?? $this->getDefaultOrderBy(),
+            $request->$orderField ?? $this->getDefaultOrder()
+        );
+    }
+
+    protected function handleTrash(Request $request, $query)
+    {
+        $model = $this->getModelClassName();
+        $uses = class_uses($model);
+        $softDeleted = 'Illuminate\Database\Eloquent\SoftDeletes';
+        if(!in_array($softDeleted, $uses)){
+            return;
+        }
+        $trashField = $this->getTrashField();
+        if($request->$trashField == true){
+            $query->onlyTrashed();
+        }
+    }
+
+    protected function handleAdditionalFiltering(Request $request, &$query){}
+
     public function show(Request $request, $modelId)
     {
-
+        $model = $this->getModelFromIdentifier($modelId);
+        $this->verifyShowAccess($model);
+        return $this->returnSingleModel($model);
     }
 
     public function store(Request $request)
     {
-
+        if(!empty($this->getStoreValidation())){
+            $request->validate($this->getStoreValidation());
+        }
+        $class = $this->getModelClassName();
+        $model = new $class();
+        if(!empty($this->getStoreFillFields())){
+            $model->fill($request->only($this->getStoreFillFields()));
+        } else {
+            $model->fill($request->all());
+        }
+        $model->save();
+        return $this->returnSingleModel($model);
     }
 
-    public function update(Request $request, $modelId)
+    public function put(Request $request, $modelId)
     {
+        if(empty($this->getPutFields())){
+            Throw new \Exception(__("You have not set any PUT fields"));
+        }
+        $model = $this->getModelFromIdentifier($modelId);
+        $this->verifyPutAccess($model);
+        if(!empty($this->getPutValidation())){
+            $request->validate($this->getPutValidation());
+        }
+        $model->fill($request->only($this->getPutFields()));
+        $model->save();
+        return $this->returnSingleModel($model);
+    }
 
+    public function patch(Request $request, $modelId)
+    {
+        if(empty($this->getPatchableFields())){
+            Throw new \Exception(__("You have not set any PATCH fields"));
+        }
+        if(!empty($this->getPatchValidation())){
+            $request->validate($this->getPatchValidation());
+        }
+        $model = $this->getModelFromIdentifier($modelId);
+        $this->verifyPatchAccess($model);
+        foreach($this->getPatchableFields() as $field){
+            $model->$field = $request->$field;
+        }
+        $model->save();
+        return $this->returnSingleModel($model);
+    }
+
+    protected function returnSingleModel($model)
+    {
+        $model->refresh();
+        $this->buildModelLinks($model);
+        return response()->json($model);
     }
 
     public function destroy(Request $request, $modelId)
     {
+        $model = $this->getModelFromIdentifier($modelId);
+        $this->verifyDeleteAccess($model);
+        $model->delete();
+        return response()->json(['message' => __('That resource has been deleted')]);
+    }
 
+    protected function getStoreValidation() : array
+    {
+        return [];
+    }
+
+    protected function getStoreFillFields() : array
+    {
+        return [];
+    }
+
+    protected function getPatchValidation() : array
+    {
+        return [];
+    }
+
+    protected function getModelFromIdentifier($identifier)
+    {
+        $class = $this->getModelClassName();
+        $model = $class::find($identifier);
+        if(empty($model)){
+            abort(404);
+        }
+        return $model;
     }
 
     protected function usePagination() : bool
     {
         return true;
+    }
+
+    protected function getTrashField() : string
+    {
+        return 'trash';
+    }
+
+    protected function getOrderField() : string
+    {
+        return 'order';
+    }
+
+    protected function getOrderByField() : string
+    {
+        return 'order_by';
+    }
+
+    protected function getDefaultOrder() : string
+    {
+        return 'DESC';
+    }
+
+    protected function getDefaultOrderBy() : string
+    {
+        return 'updated_at';
     }
 
     protected function getRecordsPerPage() : int
@@ -79,6 +227,11 @@ abstract class RestController extends Controller
     protected function getDefaultSelect() : string
     {
         return '*';
+    }
+
+    protected function getPerPageField() : string
+    {
+        return 'per_page';
     }
 
     protected function getModelInstanceFromId(int $id)
@@ -97,6 +250,26 @@ abstract class RestController extends Controller
     }
 
     protected function getFluffyFields() : array
+    {
+        return [];
+    }
+
+    protected function verifyIndexAccess(){}
+
+    protected function verifyShowAccess($model){}
+
+    protected function verifyPatchAccess($model){}
+
+    protected function verifyPutAccess($model){}
+
+    protected function verifyDeleteAccess($model){}
+
+    protected function getPatchableFields() : array
+    {
+        return [];
+    }
+
+    protected function getPutFields() : array
     {
         return [];
     }
